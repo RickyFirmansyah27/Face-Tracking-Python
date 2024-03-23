c2b5a3a51584fa49abed088debc86a971af791ef
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2024-03-23 17:40:40 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,13 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+
+#!/bin/bash
+
+
+# Install other libraries using pip
+pip install -r library.txt
+
+# Run the application
+streamlit run streamlit-app.py
+
